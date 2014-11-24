dcf363377f689b1d47617e9e2d01212e96866349
Olav Frengstad <olav@fwt.no>
2014-11-25 01:05:12 +0800

    mjau

---
+++ app.js
@@ -0,0 +1,183 @@
+var auth = 'Basic ZGVtb0B0aW55LW1lc2guY29tOksyOjEwbVMuMw==';
+angular.module('CarControl', ['tmCloudClient', 'angular-loading-bar'], function($provide) {
+      $provide.value('endpoint', 'http://31.169.50.34:8080');
+      //$provide.value('endpoint', 'http://localhost:4000');
+   })
+	.config(function($httpProvider, $provide) {
+		$httpProvider.interceptors.push(function($q) {
+			return {
+				request: function(config, headers) {
+					config.headers['Authorization'] = auth;
+					return config || $q.when(config);
+				}
+			};
+		});
+	})
+   .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+   }])
+   .controller('AppController', function($scope, $location, $q,
+         tmNet, tmMsg, tmUser, tmMsgStreamQuery) {
+
+		$scope.Math = Math;
+		$scope.user = tmUser.get();
+      $scope.collapseNets = false;
+      $scope.net = undefined;
+      $scope.$location = $location;
+		var eventsrc = undefined;
+
+		$scope.setGPIOs = function(network, device, gpio) {
+			new tmMsg({"proto/tm": {
+				"type": "command",
+			 	"command": "set_output",
+			 	"gpio": gpio
+		 	}}).$create({network: network, device: device});
+		};
+
+      $scope.setNet = function(k, nosearch) {
+			if (eventsrc) {
+				eventsrc.close();
+			};
+
+         nosearch || $location.search('network', k);
+         tmNet.get({id: k}).$promise.then(function(net) {
+            $scope.net = angular.copy(net);
+            $scope.collapseNets = true;
+         });
+      };
+
+		$scope.connect = function(mapping) {
+			// for demo lights
+			var mapping = {
+				"1XOfiz0fly": "qM109ONk0",
+				"qM109ONk0": "1XOfiz0fly",
+			};
+			var state = {
+				"1XOfiz0fly": false,
+				"qM109ONk0": false,
+			};
+			$scope.players = ["1XOfiz0fly", "qM109ONk0"];
+			$scope.state = {
+				"1XOfiz0fly": 0,
+				"qM109ONk0": 0
+			};
+			$scope.util = {
+				"1XOfiz0fly": 0,
+				"qM109ONk0": 0
+			};
+
+			$scope.eventsrc = tmMsgStreamQuery({network: $scope.net.key, authorization: auth});
+			$scope.eventsrc.addEventListener('msg', function(e) {
+				var data = JSON.parse(e.data);
+				if (data['proto/tm'] && 'io_change' === data['proto/tm'].detail) {
+				   var target = mapping[data.selector[1]];
+					state[target] = !state[target];
+					$scope.setGPIOs(data.selector[0], mapping[data.selector[1]], {"gpio_0": state[target]});
+				}
+
+				if (data['proto/tm'] && 'ima' === data['proto/tm'].detail) {
+					$scope.$apply(function() {
+						 var prev = $scope.state[data.selector[1]],
+							  diff = data['proto/tm'].data - prev;
+						 console.log(prev, data['proto/tm'].data, diff, (diff / 100) * 100);
+						 $scope.state[data.selector[1]] = data['proto/tm'].data;
+						 $scope.util[data.selector[1]] = (diff/ 100) * 100;
+					});
+				}
+			});
+		};
+
+		$scope.disconnect = function() {
+			$scope.eventsrc.close();
+			delete $scope.eventsrc;
+		};
+
+      if ($location.search().network) {
+         $scope.setNet($location.search().network, true);
+      }
+
+		 $scope.provision = function(sel) {
+         var msg = new tmMsg();
+         msg["proto/tm"] = {
+            "type": "command",
+            "command": "set_config",
+            "config": {
+               "gpio_0": {"config": 4},
+               "gpio_1": {"config": 4},
+               "gpio_4": {"config": 1, trigger: 2},
+               "gpio_6": {"config": 4},
+               "gpio_7": {"config": 3}
+            }
+           };
+
+           msg.$create({network: sel[0],
+                        device: sel[1]});
+      };
+
+		// Forover: GPIO 1 = 1, default = 0
+		// Bakover: GPIO 0 = 1,  default= 0
+		// Baklys på: GPIO 6 = 1, default = 0
+		// Dim: GPIO 7, default  = 0
+
+		$scope.car = "2cmdPzQQab";
+		$scope.putpwm = function(pwm) {
+			if (pwm < 0 || pwm > 100) return;
+
+			new tmMsg({"proto/tm": {
+				"type": "command",
+			 	"command": "set_pwm",
+			 	"pwm": Math.abs(pwm - 100),
+		 	}}).$create({network: $scope.net.key, device: $scope.car});
+		};
+
+		$scope.backward = function() {
+			$scope.setGPIOs($scope.net.key, $scope.car, {"gpio_0": true, "gpio_1": false})
+			setTimeout(function(){
+				$scope.setGPIOs($scope.net.key, $scope.car, {"gpio_0": false, "gpio_1": false})
+			}, 50);
+		};
+
+		$scope.forward = function() {
+			$scope.setGPIOs($scope.net.key, $scope.car, {"gpio_0": false, "gpio_1": true})
+			setTimeout(function(){
+				$scope.setGPIOs($scope.net.key, $scope.car, {"gpio_0": false, "gpio_1": false})
+			}, 50);
+		};
+
+   })
+   .directive('modal', function () {
+      return {
+         template: '<div class="modal fade">' +
+            '<div class="modal-dialog">' +
+               '<div class="modal-content">' +
+                  '<div class="modal-header">' +
+//                     '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                     '<h4 class="modal-title">{{ title }}</h4>' +
+                  '</div>' +
+                  '<div class="modal-body" ng-transclude></div>' +
+                  '</div>' +
+               '</div>' +
+            '</div>',
+            restrict: 'E',
+            transclude: true,
+            replace:true,
+            scope:true,
+            link: function postLink(scope, element, attrs) {
+               scope.title = attrs.title;
+
+               scope.$watch(attrs.visible, function(value) {
+                  if(value == true)
+                     $(element).modal('show');
+                  else
+                     $(element).modal('hide');
+               });
+
+               $(element).on('shown.bs.modal', function(){
+                     scope.$parent[attrs.visible] = true;
+               });
+
+               $(element).on('hidden.bs.modal', function(){
+                     scope.$parent[attrs.visible] = false;
+               });
+            }
+      };
+   });
